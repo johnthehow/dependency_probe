534d931e63c01658921f98392c061c2575ff2e73
johnthehow <johnthehow@qq.com>
2024-02-29 01:14:00 +0800

    regular

---
+++ scripts4/dataset.py
@@ -0,0 +1,258 @@
+from transformers import BertModel, BertTokenizer
+from config import *
+from thehow.wordpiece2token.mapper import idx_mapper, matrix_denser, matrix_clssep_stripper
+from thehow.tuda.depd_core import trees_gi
+from collections import namedtuple, defaultdict
+import torch
+from torch.utils.data import Dataset, DataLoader
+import pickle
+from torch import nn
+import time
+
+class timer(object):
+	def __enter__(self):
+		self.time_start = time.time()
+	def __exit__(self,exc_type, exc_val,exc_tb):
+		self.time_end = time.time()
+		print(f'duration: {self.time_end - self.time_start}')
+
+
+model = BertModel.from_pretrained(MODEL_PATH)
+model.to('cuda')
+tokenizer = BertTokenizer.from_pretrained(MODEL_PATH)
+
+def save_treeobjs():
+	for conll_path in [TRAIN_CONLL_PATH, DEV_CONLL_PATH, TEST_CONLL_PATH]:
+		tgi = trees_gi(conll_path)
+		trees = [tree for tree in tgi]
+		res_trees = []
+		raw_tree_cnt = 0
+		print(f'[DATASET] filtering mappable trees...')
+		for tree in trees:
+			print(f'prcessing {raw_tree_cnt}-th sent', end='\x1b\r')
+			text = tree.text_lower
+			user_tksent = [node.token.lower() for node in tree.nodes]
+			bert_tksent = tokenizer(text, return_tensors='pt')
+			wdps_tksent = tokenizer.convert_ids_to_tokens(bert_tksent['input_ids'][0][1:-1])
+			idxmap = idx_mapper(wdps_tksent, user_tksent)
+			if idxmap != None: # 筛选， 只允许可合并wordpiece的句子
+				res_trees.append(tree)
+			else:
+				pass
+			raw_tree_cnt += 1
+		print(f'[DATASET] total trees mappable: {len(res_trees)}')
+		print(f'[DATSSET] pickling mappable trees as tuda tree objects...')
+		save_path = DATASET_PKL_PATH.joinpath('treeobjs').joinpath(f'MAPPABLE_TREEOBJS[DATA]{conll_path.stem}[MODEL]{MODEL_NAME}.pkl')
+		with open(save_path, mode='wb') as file:
+			pickle.dump(res_trees, file)
+		print(f'[DATASET] treeobjs pickled at {save_path}')
+	return
+
+def save_depdmats():
+	for conll_path in [TRAIN_CONLL_PATH, DEV_CONLL_PATH, TEST_CONLL_PATH]:
+		treeobjs_path = DATASET_PKL_PATH.joinpath('treeobjs').joinpath(f'MAPPABLE_TREEOBJS[DATA]{conll_path.stem}[MODEL]{MODEL_NAME}.pkl')
+		with open(treeobjs_path, mode='rb') as file:
+			treeobjs = pickle.load(file)
+		depd_mats = []
+		tree_cnt = 0
+		print(f'[DATASET] generating depd mats for mappable trees...')
+		for tree in treeobjs:
+			print(f'prcessing {tree_cnt}-th tree', end='\x1b\r')
+			depd_mats.append(torch.tensor(tree.ajacency_matrix_weighted_absolute_full))
+			tree_cnt += 1
+		save_path = DATASET_PKL_PATH.joinpath('depdmats').joinpath(f'DEPDMATS[DATA]{conll_path.stem}[MODEL]{MODEL_NAME}.pkl')
+		print(f'[DATSSET] pickling depd mats...')
+		with open(save_path, mode='wb') as file:
+			pickle.dump(depd_mats, file)
+		print(f'[DATASET] depd mats pickled at {save_path}')
+	return
+
+def save_lengths():
+	for conll_path in [TRAIN_CONLL_PATH, DEV_CONLL_PATH, TEST_CONLL_PATH]:
+		treeobjs_path = DATASET_PKL_PATH.joinpath('treeobjs').joinpath(f'MAPPABLE_TREEOBJS[DATA]{conll_path.stem}[MODEL]{MODEL_NAME}.pkl')
+		with open(treeobjs_path, mode='rb') as file:
+			treeobjs = pickle.load(file)
+		lengths = []
+		tree_cnt = 0
+		for tree in treeobjs:
+			print(f'[DATASET] processing {tree_cnt}-th tree', end='\x1b\r')
+			lengths.append(tree.len)
+			tree_cnt += 1
+		save_path = DATASET_PKL_PATH.joinpath('lengths').joinpath(f'LENGTHS[DATA]{conll_path.stem}[MODEL]{MODEL_NAME}.pkl')
+		print(f'[DATSSET] pickling lengths...')
+		with open(save_path, mode='wb') as file:
+			pickle.dump(lengths, file)
+		print(f'[DATASET] lengths pickled at {save_path}')
+	return
+
+def save_embeddings():
+	print(f'[DATASET] calculating and saving embeddings')
+	for conll_path in [TRAIN_CONLL_PATH, DEV_CONLL_PATH, TEST_CONLL_PATH]:
+		treeobjs_path = DATASET_PKL_PATH.joinpath('treeobjs').joinpath(f'MAPPABLE_TREEOBJS[DATA]{conll_path.stem}[MODEL]{MODEL_NAME}.pkl')
+		with open(treeobjs_path, mode='rb') as file:
+			treeobjs = pickle.load(file)
+		tree_cnt = 0
+		texts = []
+		lengths_token = []
+		user_tksents = []
+		bert_tksents = []
+		wdps_tksents = []
+		lengths_wdps = []
+		idxmaps = []
+		for tree in treeobjs:
+			print(f'[DATASET] processing {tree_cnt}-th sent', end='\x1b\r')
+			text = tree.text_lower
+			texts.append(text)
+			lengths_token.append(tree.len)
+			user_tksent = [node.token.lower() for node in tree.nodes]
+			user_tksents.append(user_tksent)
+			bert_tksent = tokenizer(text, return_tensors='pt')
+			bert_tksents.append(bert_tksent)
+			wdps_tksent = tokenizer.convert_ids_to_tokens(bert_tksent['input_ids'][0][1:-1])
+			wdps_tksents.append(wdps_tksent)
+			lengths_wdps.append(len(wdps_tksent)+2)
+			idxmap = idx_mapper(wdps_tksent, user_tksent)
+			idxmaps.append(idxmap)
+			tree_cnt += 1
+		print(f'tokenizing with bert tokenizer')
+		batch_tksents = tokenizer(texts, padding=True, return_tensors='pt')
+		batch_tksents.to('cuda')
+		class embed_dataset(Dataset):
+			def __init__(self, tksents):
+				self.tksents = tksents
+				self.input_ids = self.tksents['input_ids']
+				self.token_type_ids = self.tksents['token_type_ids']
+				self.attention_mask = self.tksents['attention_mask']
+			def __len__(self):
+				return len(self.input_ids)
+			def __getitem__(self,idx):
+				seg = dict(input_ids = self.input_ids[idx], token_type_ids = self.token_type_ids[idx], attention_mask = self.attention_mask[idx])
+				return seg
+		print(f'instantiating dataset object')
+		embed_ds = embed_dataset(batch_tksents)
+		embed_dl = DataLoader(embed_ds, batch_size = 800, shuffle=False)
+		embeds = defaultdict(list)
+		batch_cnt = 0
+		for batch in embed_dl:
+			with timer() as tm:
+				print(f'calculating {batch_cnt}-th batch')
+				with torch.no_grad():
+					batch_output = model(**batch, output_hidden_states = True)
+				batch_cnt += 1
+				torch.cuda.empty_cache()
+				for i in range(MODEL_NLAYER+1):
+					embeds[i].append(batch_output['hidden_states'][i].to('cpu'))
+		embeds_cats = [torch.cat(embeds[i], axis=0) for i in range(MODEL_NLAYER+1)]
+		embeds_cats_zigzag = defaultdict(list)
+		print('densing and saving embeddings')
+		for layer in range(MODEL_NLAYER+1):
+			print(f'layer {layer}')
+			for idx, mat in enumerate(embeds_cats[layer]):
+				raw_sent_mat = mat[:lengths_wdps[idx]]
+				stripmat = matrix_clssep_stripper(raw_sent_mat, scale=False)
+				densemat = matrix_denser(stripmat, idxmaps[idx], rowwise=True)
+				embeds_cats_zigzag[i].append(densemat)
+		return
+
+def load_treeobjs():
+	print(f'[DATASET] loading treeobjs for {TRAIN_CONLL_PATH.stem}, {DEV_CONLL_PATH.stem} and {TEST_CONLL_PATH.stem}')
+	treeobjs = []
+	for conll_path in [TRAIN_CONLL_PATH, DEV_CONLL_PATH, TEST_CONLL_PATH]:
+		filename =  DATASET_PKL_PATH.joinpath('treeobjs').joinpath(f'MAPPABLE_TREEOBJS[DATA]{conll_path.stem}[MODEL]{MODEL_NAME}.pkl')
+		with open(filename, mode='rb') as file:
+			treeobjs.append(pickle.load(file))
+	return treeobjs
+
+def load_depdmats():
+	print(f'[DATASET] loading depdmats for {TRAIN_CONLL_PATH.stem}, {DEV_CONLL_PATH.stem} and {TEST_CONLL_PATH.stem}')
+	depdmats = []
+	for conll_path in [TRAIN_CONLL_PATH, DEV_CONLL_PATH, TEST_CONLL_PATH]:
+		filename =  DATASET_PKL_PATH.joinpath('depdmats').joinpath(f'DEPDMATS[DATA]{conll_path.stem}[MODEL]{MODEL_NAME}.pkl')
+		with open(filename, mode='rb') as file:
+			depdmats.append(pickle.load(file))
+	return depdmats
+
+def load_lengths():
+	print(f'[DATASET] loading lengths for {TRAIN_CONLL_PATH.stem}, {DEV_CONLL_PATH.stem} and {TEST_CONLL_PATH.stem}')
+	lengths = []
+	for conll_path in [TRAIN_CONLL_PATH, DEV_CONLL_PATH, TEST_CONLL_PATH]:
+		filename =  DATASET_PKL_PATH.joinpath('lengths').joinpath(f'LENGTHS[DATA]{conll_path.stem}[MODEL]{MODEL_NAME}.pkl')
+		with open(filename, mode='rb') as file:
+			lengths.append(pickle.load(file))
+	return lengths
+
+def load_embeddings():
+	print(f'[DATASET] loading embeddings for {TRAIN_CONLL_PATH.stem}, {DEV_CONLL_PATH.stem} and {TEST_CONLL_PATH.stem}')
+	embeddings = []
+	for conll_path in [TRAIN_CONLL_PATH, DEV_CONLL_PATH, TEST_CONLL_PATH]:
+		filename =  DATASET_PKL_PATH.joinpath('embeddings').joinpath(f'EMBEDDINGS[MODEL]{MODEL_NAME}[DATA]{conll_path.stem}[LAYER]{HIDDEN_LAYER}.pkl')
+		with open(filename, mode='rb') as file:
+			embeddings.append(pickle.load(file))
+	return embeddings
+
+def assemble_datasets():
+	class mydataset(Dataset):
+		def __init__(self, observations):
+			self.observations = observations
+		def __len__(self):
+			return len(self.observations)
+		def __getitem__(self,idx):
+			return self.observations[idx]
+		
+	def custom_pad(batch_observations):
+		seqs = [x[0].embeddings for x in batch_observations]
+		lengths = torch.tensor([len(x) for x in seqs])
+		seqs = nn.utils.rnn.pad_sequence(seqs, batch_first=True)
+		label_shape = batch_observations[0][1].shape
+		maxlen = int(max(lengths))
+		label_maxshape = [maxlen for x in label_shape]
+		labels = [-torch.ones(*label_maxshape) for x in seqs]
+		for index, x in enumerate(batch_observations):
+			length = x[1].shape[0]
+			if len(label_shape) == 1:
+				labels[index][:length] = x[1]
+			elif len(label_shape) == 2:
+				labels[index][:length,:length] = x[1]
+			else:
+				raise ValueError("Labels must be either 1D or 2D right now; got either 0D or >3D")
+		labels = torch.stack(labels)
+		return seqs, labels, lengths, batch_observations
+	
+	observation_class = namedtuple('Observation', ['index', 'sentence', 'lemma_sentence', 'upos_sentence', 'xpos_sentence', 'morph', 'head_indices', 'governance_relations', 'secondary_relations', 'extra_info', 'embeddings'])
+
+	treeobjs_triad = load_treeobjs()
+	depdmats_triad = load_depdmats()
+	embeddings_triad = load_embeddings()
+	dataloaders_triad = []
+	print(f'[DATASET] assembling observations for {TRAIN_CONLL_PATH.stem}, {DEV_CONLL_PATH.stem} and {TEST_CONLL_PATH.stem}')
+	for treeobjs, depdmats, embeddings in zip(treeobjs_triad, depdmats_triad, embeddings_triad):
+		observations = []
+		for idx, tree in enumerate(treeobjs):
+			obs_nodes = tree.nodes
+			obs_index = tuple([i.id for i in obs_nodes])
+			obs_sentence = tuple([i.token for i in obs_nodes])
+			obs_lemma_sentence = tuple([i.lemma for i in obs_nodes])
+			obs_upos = tuple([i.upos for i in obs_nodes])
+			obs_xpos = tuple([i.xpos for i in obs_nodes])
+			obs_morph = tuple([i.feats for i in obs_nodes])
+			obs_head_indices = tuple([i.headid for i in obs_nodes])
+			obs_governance_relations = tuple([i.deprel for i in obs_nodes])
+			obs_secondary_relations = tuple([i.deps for i in obs_nodes])
+			obs_extra_info = tuple([i.misc for i in obs_nodes])
+			densemat = embeddings[idx].type(torch.float32)
+			depdmat = depdmats[idx].type(torch.float32)
+			tree_observation = observation_class(obs_index,obs_sentence,obs_lemma_sentence,obs_upos,obs_xpos,obs_morph,obs_head_indices,obs_governance_relations, obs_secondary_relations,obs_extra_info, densemat)
+			observations.append((tree_observation,depdmat))
+		ds = mydataset(observations)
+		dl = DataLoader(ds,batch_size=BATCH_SIZE,collate_fn=custom_pad, shuffle=False)
+		dataloaders_triad.append(dl)
+		print(f'[DATASET] dataloader instantiated: observations: {len(dl.dataset)}, batches: {len(dl)}')
+	return dataloaders_triad
+
+# dataloaders_triad = assemble_datasets()
+
+if __name__ == '__main__':
+	# save_treeobjs()
+	# save_depdmats()
+	# save_lengths()
+	save_embeddings()
---
+++ scripts4/report.py
@@ -0,0 +1,172 @@
+from collections import defaultdict
+from scipy.stats import spearmanr
+import numpy as np
+from config import *
+import config
+
+'''
+    prediction_batches: # list @ batch数量 [0,...,N_BATCH] # ndarray @(batch_size, 最大句长， 最大句长)
+    dataset: # dataloader @batch数量 
+      [0]: # tuple @ 4 
+        [0]: * feat_batch # tensor @(batch_size, 最大句长，1024) 
+        [1]: * lab_batch # tensor @(batch_size, 最大句长，最大句长） 
+        [2]: * len_batch # tensor @ (batch_size,) 
+        [3]: * obs_batch 一批句子 conll + feat_embeddings # list @ batch_size 
+          [0,1,2,.., 39]: # tuple @ 2
+            [0]: # observation<-namedtuple 
+              .embeddings: * 内容同dataset[0]中一句话的内容，但是行数等于句长而不是批最大句长 @ （句长，1024)
+              .extra_info
+              .governance_relations
+              .head_idx
+              .index
+              .lemma_sentence
+              .morph
+              .secondary_relations
+              .sentence
+              .upos
+              .xpos
+            [1]: * 句子中两次之间的路径距离， 内容类似dataset[1],但是行数等于句长而不是批最大句长 # tensor @ (句长，句长)
+    '''
+
+def report_spearmanr(probe_trained, dataset):
+    prediction_batches = []
+    probe = probe_trained[0]
+    for feat_batch, lab_batch, length_batch, observation_batch in dataset:
+        pred = probe(feat_batch)
+        prediction_batches.append(pred)
+    lengths_to_spearmanrs = defaultdict(list)
+    for prediction_batch, (data_batch, label_batch, length_batch, observation_batch) in zip(prediction_batches, dataset):
+        for prediction, label, length, (observation, _) in zip(prediction_batch, label_batch, length_batch, observation_batch):
+            words = observation.sentence
+            length = int(length)
+            prediction = prediction[:length,:length].detach()
+            label = label[:length,:length].cpu()
+            spearmanrs = [spearmanr(pred, gold) for pred, gold in zip(prediction, label)]
+            lengths_to_spearmanrs[length].extend([x.correlation for x in spearmanrs])
+    mean_spearman_for_each_length = {length: np.mean(lengths_to_spearmanrs[length]) for length in lengths_to_spearmanrs}
+    mean_spearman_for_sents_len_5_50 = np.mean([mean_spearman_for_each_length[x] for x in range(5,51) if x in mean_spearman_for_each_length])
+
+    return mean_spearman_for_each_length, mean_spearman_for_sents_len_5_50
+
+def report_writer(probe_trained, dataset):
+    print(f'[REPORT] writing reports...')
+    spearmanr_res = report_spearmanr(probe_trained, dataset)
+    mean_spearman_for_each_length = spearmanr_res[0]
+    mean_spearman_for_sents_len_5_50 = spearmanr_res[1]
+    uuas_res = report_uuas(probe_trained, dataset)
+    report_filepath = REPORTS_PATH.joinpath(f'REPORT[COR]SPEARMANR[ACC]UUAS[MODEL]{MODEL_NAME}[LAYER]{HIDDEN_LAYER}[EPOCHS]{EPOCHS}[BATCHSIZE]{BATCH_SIZE}[LR]{LEARNING_RATE}[LOSSPOW]{PROBE_LOSS_POW}[METHOD]{METHOD}{CONLL_ALIAS}[DIRECTED]{str(DEPD_DIRECTED).lower()}.txt')
+    epoch_losses = probe_trained[1]
+    with open(report_filepath, 'w') as fout:
+        fout.write(f'[Mean spearmanr for sents of len 5-50]\n')
+        fout.write(f'{str(mean_spearman_for_sents_len_5_50)}\n')
+        fout.write(f'\n[UUAS]\n')
+        fout.write(f'{uuas_res}\n')
+        fout.write(f'\n[SPEARMANR]\n')
+        fout.write(f'sent_len\tmean_spearmanr\n')
+        for length in sorted(mean_spearman_for_each_length):
+            fout.write(str(length) + '\t' + str(mean_spearman_for_each_length[length]) + '\n')
+        fout.write(f'\n[EPOCH LOSSES]\n')
+        for i in epoch_losses:
+           fout.write(f'{i}\n')
+        fout.write(f'\n[SETTINGS]\n')
+        for k,v in (config.__dict__).items():
+             if k.isupper() == True:
+                 fout.write(f'{k}\t{str(v)}\n')
+    print(f'[REPORT] report saved at {report_filepath}')
+    return
+ 
+
+
+
+class UnionFind:
+  '''
+  Naive UnionFind implementation for (slow) Prim's MST algorithm
+
+  Used to compute minimum spanning trees for distance matrices
+  '''
+  def __init__(self, n):
+    self.parents = list(range(n))
+  def union(self, i,j):
+    if self.find(i) != self.find(j):
+      i_parent = self.find(i)
+      self.parents[i_parent] = j
+  def find(self, i):
+    i_parent = i
+    while True:
+      if i_parent != self.parents[i_parent]:
+        i_parent = self.parents[i_parent]
+      else:
+        break
+    return i_parent
+
+def prims_matrix_to_edges(matrix, words, poses):
+  '''
+  Constructs a minimum spanning tree from the pairwise weights in matrix;
+  returns the edges.
+
+  Never lets punctuation-tagged words be part of the tree.
+  '''
+  pairs_to_distances = {}
+  uf = UnionFind(len(matrix))
+  for i_index, line in enumerate(matrix):
+    for j_index, dist in enumerate(line):
+      if poses[i_index] in ["''", ",", ".", ":", "``", "-LRB-", "-RRB-"]:
+        continue
+      if poses[j_index] in ["''", ",", ".", ":", "``", "-LRB-", "-RRB-"]:
+        continue
+      pairs_to_distances[(i_index, j_index)] = dist
+  edges = []
+  for (i_index, j_index), distance in sorted(pairs_to_distances.items(), key = lambda x: x[1]):
+    if uf.find(i_index) != uf.find(j_index):
+      uf.union(i_index, j_index)
+      edges.append((i_index, j_index))
+  return edges
+
+
+def report_uuas(probe_trained, dataset):
+    '''
+    prediction_batches: # list @ batch数量 [0] # ndarray @(batch_size, 最大句长， 最大句长)
+    dataset: # dataloader @batch数量 
+        [0]: # tuple @ 4 
+        [0]: * feat_batch # tensor @(batch_size, 最大句长，1024) 
+        [1]: * lab_batch # tensor @(batch_size, 最大句长，最大句长） 
+        [2]: * len_batch # tensor @ (batch_size,) 
+        [3]: * obs_batch 一批句子 conll + feat_embeddings # list @ batch_size 
+            [0,1,2,.., 39]: # tuple @ 2
+            [0]: # observation<-namedtuple 
+                .embeddings: * 内容同dataset[0]中一句话的内容，但是行数等于句长而不是批最大句长 @ （句长，1024)
+                .extra_info
+                .governance_relations
+                .head_idx
+                .index
+                .lemma_sentence
+                .morph
+                .secondary_relations
+                .sentence
+                .upos
+                .xpos
+            [1]: * 句子中两次之间的路径距离， 内容类似dataset[1],但是行数等于句长而不是批最大句长 # tensor @ (句长，句长)
+    '''
+    prediction_batches = []
+    probe = probe_trained[0]
+    for feat_batch, lab_batch, length_batch, observation_batch in dataset:
+        pred = probe(feat_batch)
+        prediction_batches.append(pred)
+    uspan_total = 0
+    uspan_correct = 0
+    total_sents = 0
+    for prediction_batch, (data_batch, label_batch, length_batch, observation_batch) in zip(prediction_batches, dataset):
+        for prediction, label, length, (observation, _) in zip(prediction_batch, label_batch,length_batch, observation_batch):
+            words = observation.sentence
+            poses = observation.xpos_sentence
+            length = int(length)
+            assert length == len(observation.sentence)
+            prediction = prediction[:length,:length]
+            label = label[:length,:length].cpu()
+            gold_edges = prims_matrix_to_edges(label, words, poses)
+            pred_edges = prims_matrix_to_edges(prediction, words, poses)
+            uspan_correct += len(set([tuple(sorted(x)) for x in gold_edges]).intersection(set([tuple(sorted(x)) for x in pred_edges])))
+            uspan_total += len(gold_edges)
+            total_sents += 1
+    uuas = uspan_correct / float(uspan_total)
+    return uuas
